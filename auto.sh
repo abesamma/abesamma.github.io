0848a6d687cf29bf2bb4de49b29cb2c5f72c8df5
Abraham Samma <sammadiscerning@gmail.com>
2019-06-24 18:48:05 +0300

    Add bash script to automate deployments

---
+++ auto.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+do_deploy() {
+    while true; do
+        read -p "Do you wish to deploy to github? [Y/N] " deploy
+        case $deploy in
+            [Yy]* ) echo "Pushing to github"
+            git push -u origin master; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer Yy (yes) or Nn (no) only.";
+        esac
+    done
+}
+
+do_commit() {
+    while true; do
+        read -p "Do you wish to commit the work? [Y/N] " commit
+        case $commit in
+            [Yy]* ) read -p "Type commit message " msg
+            echo "Committing..."
+            git add .
+            git commit -m "$msg"
+            do_deploy; break;;
+            [Nn]* ) do_deploy; break;;
+            * ) echo "Please answer Yy (yes) or Nn (no) only.";
+        esac
+    done
+}
+
+increment_version() {
+    version=$(jq .version version.json)
+    let version=version+1
+    echo '{ "version": '$version' }' > version.json
+}
+
+while true; do
+    read -p "Do you wish to increment the version? [Y/N] " increment
+    case $increment in
+        [Yy]* ) increment_version
+        do_commit; break;;
+        [Nn]* ) do_commit; break;;
+        * ) echo "Please answer Yy (yes) or Nn (no) only.";
+    esac
+done
